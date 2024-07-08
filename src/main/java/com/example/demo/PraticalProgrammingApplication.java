dde7501659394cf6e4b172182f2e5449684d32af
nyoongoon <nyoongoon@naver.com>
2024-07-08 12:11:49 +0900

    VO  불변성, 동등성, 자가검증  핵심은 신뢰할 수 있는 객체 여부

---
+++ src/main/java/com/example/demo/PraticalProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticalProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraticalProgrammingApplication.class, args);
+    }
+
+}
